48015728ed03d2ae00e00465751fce1782707c7e
alec <alec.hulce@gmail.com>
2014-10-30 01:08:01 -0700

    u

--- test/test.js
+++ test/test.js
@@ -7,9 +7,10 @@ test('levs',function(t){
 	,dict = __dirname+'/../dict.example'
 	;
 	console.log(['Host: '+host, 'Dictionary: '+dict, '\n'].join('\n'));
-	level.saveResult(host,'ralph','password',dict,1,function(err,data){
-		level.saveResult(host,'ralph','password2',dict,0,function(err,data){
-			level.getResultsForHostUser(host,'ralph',function(err,data){
+
+	level.saveResult(host, 'ralph', 'password', dict, 1, function(err,data){
+		level.saveResult(host, 'ralph', 'password2', dict, 0, function(err,data){
+			level.getResultsForHostUser(host, 'ralph', function(err,data){
 				console.log(err,data);
 				t.equal(data.length, 2, 'level worky')
 				t.end();
